914c756b0183bde46ba55c8e84bae8c7ee484d1b
Lucas.Pereira <tilucasgap13@gmail.com>
2023-03-04 15:53:22 -0300

    modified:   scripts/controller/CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -34,6 +34,7 @@ class CalcController {
     // Apaga TODOS os dados da tela
     clearAll() {
         this._operation = [];
+        this.displayCalc = "";
     }
     // Apaga ultima entrada
     clearEntry() {
@@ -70,12 +71,37 @@ class CalcController {
     pushOperation(value){
 
         this._operation.push(value);
+
         if (this._operation.length > 3){
 
-            let last = this._operation.pop;
-            console.log(this._operation);
+            this.calc();
 
         } 
+
+    }
+    calc(){
+
+        let last = this._operation.pop(); 
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
+
+    }
+    setLastNumberToDisplay(){
+
+        let lastNumber;
+
+         for (let i = this._operation.length-1; i >= 0; i--){
+
+            if (!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+        this.displayCalc = lastNumber;
 
     }
     // Adiciona nova entrada
@@ -95,6 +121,8 @@ class CalcController {
             }else {
 
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
+
 
             }
 
@@ -108,11 +136,12 @@ class CalcController {
                 let newValue = this.getLastOperation().toString() + value.toString();
                 this.setLastOperation(parseInt(newValue));
 
+                //atualizar display
+                this.setLastNumberToDisplay();
             }
 
             
         }
-
 
     }
     // Mensagem de erro
