386ff642687b2c17e8c9e8e8c3c892f454294cbf
Lucas.Pereira <tilucasgap13@gmail.com>
2023-03-11 20:51:38 -0300

    modified:   scripts/controller/CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -13,6 +15,7 @@ class CalcController {
         this.initialize();
         this.initButtonsEvent();
         this.initKeyboard();
+        
     }
     copyToClipboard(){
         let input = document.createElement('input');
@@ -46,12 +49,30 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteToClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+            })
+        })
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.currentTime =0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() {
 
         document.addEventListener('keyup', e => {
-            console.log(e.key);
+            
+            this.playAudio();
 
             switch (e.key) {
                 case 'Escape':
@@ -156,8 +177,14 @@ class CalcController {
     }
 
     getResult() {
-
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(()=>{
+                this.setError();
+            },1);
+        }
+        
     }
 
     calc() {
@@ -283,6 +310,8 @@ class CalcController {
     // executa os botões
     execBtn(value) {
 
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                 this.clearAll();
@@ -382,6 +411,11 @@ class CalcController {
 
     }
     set displayCalc(value) {
+
+        if (value.toString().length > 10){
+            this.setError();
+            return false;
+        }
         this._displayCalcEl.innerHTML = value;
 
     }
